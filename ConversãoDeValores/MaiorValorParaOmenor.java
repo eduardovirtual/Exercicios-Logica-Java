b6bb59912e5e677ebb1cf13b0c187c09ec4fe1df
Eduardo dos Santos da Costa <127545505+eduardovirtual@users.noreply.github.com>
2023-06-14 18:04:14 -0300

    Add files via upload

---
+++ ConversãoDeValores/MaiorValorParaOmenor.java
@@ -0,0 +1,12 @@
+package ConversãoDeValores;
+
+public class MaiorValorParaOmenor {
+	public static void main(String[]args) {
+		double valorDouble = 9.78d;
+		int valorInteiro = (int) valorDouble;
+		
+		System.out.println(valorDouble);
+		System.out.println(valorInteiro);
+	}
+
+}
